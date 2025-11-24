da01ff1dd6d3b155c6ed118226f567a29a5258cb
ramde solanki <ramdesolanki2511@gmail.com>
2025-11-24 07:02:50 +0530

    home page animation add

--- src/components/common/Footer/Contact/index.js
+++ src/components/common/Footer/Contact/index.js
@@ -1,11 +1,14 @@
 import { BlocksRenderer } from '@strapi/blocks-react-renderer'
 import styles from './contact.module.css'
+import FadeUp from '@/components/ui/animations/FadeUp'
 
 export default function ContctDetail({ extraDetails }) {
   return (
     <div className={styles.footerCta}>
         {extraDetails.map((item, index) => <div key={index} className={styles.footerCol}>
+      <FadeUp>
             <BlocksRenderer content={item.Content} />
+        </FadeUp>
         </div>)}
     </div>
   )
--- src/components/common/Footer/SocialLinks/index.js
+++ src/components/common/Footer/SocialLinks/index.js
@@ -3,12 +3,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import Image from 'next/image';
 import styles from './socialLinks.module.css'
+import FadeUp from '@/components/ui/animations/FadeUp';
 
 export default function SocialLinks({ socialLinks }) {
   return (
     <div className={styles.social}>
         <ul>
         {socialLinks.map((item, index) => <li key={index}>
+          <FadeUp>
             <Link href={item.Links.ButtonURL}>
             <Swiper className="mySwiper">
                 {item.Media.ImageORCarousel.map((item, index) => <SwiperSlide key={index}>
@@ -22,6 +24,7 @@ export default function SocialLinks({ socialLinks }) {
             </Swiper>
             <div className={styles.font12}>{item.Links.ButtonText}</div>
             </Link>
+            </FadeUp>
         </li>)}
         </ul>
     </div>
