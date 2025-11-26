00c3b2d2e645d454c64741e94d0fd6e0769e9be0
ramde solanki <ramdesolanki2511@gmail.com>
2025-11-26 23:31:51 +0530

    aesthetics page created

--- src/app/aesthetics/[slug]/page.js
+++ src/app/aesthetics/[slug]/page.js
@@ -33,8 +33,10 @@ export default function AestheticsDetail() {
   const [screenWidth, setScreenWidth] = useState(0);
   const [showNav, setShowNav] = useState(true);
   const sections = ["home", "about", "services", "contact"];
+  const [displayData, setDisplayData] = useState(null);
+  const [incomingData, setIncomingData] = useState(null);
+  const [showInnerContent, setShowInnerContent] = useState(false);
   const animationRef = useRef(null);
-  const [prevSlug, setPrevSlug] = useState(null);
 
   const { data, loading, error } = useQuery(ASTHETICS_QUERY_SLUG, {
     fetchPolicy: "cache-first",
@@ -46,14 +48,10 @@ export default function AestheticsDetail() {
   const activeData = astheticsData.find((item) => item.Slug === targetSlug);
 
   useEffect(() => {
-  setPrevSlug((old) => (old === targetSlug ? old : old || targetSlug));
-
-  if (prevSlug !== targetSlug) {
-    setTimeout(() => {
-      setPrevSlug(targetSlug);  
-    }, 800);
-  }
-}, [targetSlug]);
+    if (activeData && !displayData) {
+      setDisplayData(activeData);
+    }
+  }, [activeData]);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -109,154 +107,179 @@ export default function AestheticsDetail() {
     window.dispatchEvent(new Event("theme-change"));
   }, []);
 
+  useEffect(() => {
+    if (!activeData) return;
+
+    setIncomingData(activeData);
+
+    const timer = setTimeout(() => {
+      setDisplayData(activeData);
+      setIncomingData(null);
+    }, 800);
+
+    return () => clearTimeout(timer);
+  }, [activeData]);
+
   if (loading && !astheticsData.length) return <Loading />;
   if (error) return <p>Error loading data</p>;
 
-  const variants = {
-    initial: { y: "100%", zIndex: 10 },
-    animate: { y: "0%", zIndex: 10 },
-    // exit: {
-    //   y: "0%",
-    //   zIndex: 1,
-    //   opacity: 1,
-    // },
-  };
+  useEffect(() => {
+    if (!incomingData) return;
+
+    setShowInnerContent(false);
+
+    const t = setTimeout(() => {
+      setShowInnerContent(true);
+    }, 300);
+
+    return () => clearTimeout(t);
+  }, [incomingData]);
+
+  const renderContent = (activeData) => (
+    <motion.div
+      ref={containerRef}
+      className={styles.container}
+      drag="x"
+      dragConstraints={{ left: -maxDrag, right: 0 }}
+      style={{ x }}
+    >
+      <section className={`${styles.section} ${styles.one}`}>
+        <div className={styles.backgroundWrapper}>
+          {activeData?.DesktopMedia.EnableMuxVideo &&
+            activeData?.DesktopMedia.MuxVideo?.playback_id && (
+              <MuxPlayer
+                playbackId={activeData?.DesktopMedia.MuxVideo.playback_id}
+                streamType="on-demand"
+                autoPlay
+                muted
+                loop
+                playsInline
+                preload="auto"
+                style={{
+                  aspectRatio: "16 / 9",
+                  width: "100%",
+                  height: "100%",
+                }}
+              />
+            )}
+          {!activeData?.DesktopMedia?.EnableMuxVideo &&
+            Array.isArray(activeData?.DesktopMedia?.ImageORCarousel) &&
+            activeData?.DesktopMedia.ImageORCarousel.length === 1 && (
+              <Image
+                src={activeData?.DesktopMedia.ImageORCarousel[0]?.url}
+                alt={
+                  activeData?.DesktopMedia.ImageORCarousel[0]
+                    ?.alternativeText || ""
+                }
+                width={1905}
+                height={1271}
+                className={styles.mainMedia}
+              />
+            )}
+        </div>
+        <div className={styles.overlay}></div>
+        <motion.div
+          className={styles.content}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: showInnerContent ? 1 : 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <div className={`${styles.child} ${styles.topLeft}`}>
+            <ul className={styles.topNav}>
+              <li>
+                <Link href={"#"}>OVERVIEW</Link>
+              </li>
+              <li>
+                <Link href={"#"}>MATERIAL APPROACH</Link>
+              </li>
+              <li>
+                <Link href={"#"}>COLOURS AND TONES</Link>
+              </li>
+              <li>
+                <Link href={"#"}>RELATED AESTHETICS </Link>
+              </li>
+            </ul>
+          </div>
+          <div className={`${styles.child} ${styles.topRight}`}>
+            <Paragraph>
+              <BlocksRenderer content={activeData.Description || []} />
+            </Paragraph>
+          </div>
+          <div className={`${styles.child} ${styles.bottomLeft}`}>
+            <div className={`${styles.navItem}`}>
+              {astheticsData.map((item) => {
+                const isActive = pathname === `/aesthetics/${item.Slug}`;
+                return (
+                  <Link
+                    href={item.Slug}
+                    className={isActive ? "activeLink" : "normalLink"}
+                  >
+                    <span className={styles.icon}>
+                      {isActive ? "(•)" : "( )"}
+                    </span>
+                    <span className={styles.label}>{item.Name}</span>
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+          <div className={`${styles.child} ${styles.bottomRight}`}>
+            <LinkWithArrow text={"SCROLL"} href={"#"} />
+          </div>
+          <div className={`${styles.child} ${styles.center}`}>
+            <Heading level={1} className={styles.mainHeading}>
+              {activeData.Name}
+            </Heading>
+          </div>
+        </motion.div>
+      </section>
+      {activeData.Blocks.map((block, index) => (
+        <section key={index} className={`${styles.section} media-section`}>
+          <BlockRenderer block={block} />
+        </section>
+      ))}
+    </motion.div>
+  );
 
   return (
-    <div
-      className={styles.wrapper}
-      style={{
-        position: "relative",
-        height: "100vh",
-        overflow: "hidden",
-        width: "100vw",
-      }}
-    >
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={targetSlug}
-          variants={variants}
-          initial="initial"
-          animate="animate"
-          exit={{ y: 0 }}
-          transition={{ duration: 0.8, ease: [0.22, 1, 0.36, 1] }}
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          {activeData ? (
-            <motion.div
-              ref={containerRef}
-              className={styles.container}
-              drag="x"
-              dragConstraints={{ left: -maxDrag, right: 0 }}
-              style={{ x }}
-            >
-              <section className={`${styles.section} ${styles.one}`}>
-                <div className={styles.backgroundWrapper}>
-                  {activeData?.DesktopMedia.EnableMuxVideo &&
-                    activeData?.DesktopMedia.MuxVideo?.playback_id && (
-                      <MuxPlayer
-                        playbackId={
-                          activeData?.DesktopMedia.MuxVideo.playback_id
-                        }
-                        streamType="on-demand"
-                        autoPlay
-                        muted
-                        loop
-                        playsInline
-                        preload="auto"
-                        style={{
-                          aspectRatio: "16 / 9",
-                          width: "100%",
-                          height: "100%",
-                        }}
-                      />
-                    )}
-                  {!activeData?.DesktopMedia?.EnableMuxVideo &&
-                    Array.isArray(activeData?.DesktopMedia?.ImageORCarousel) &&
-                    activeData?.DesktopMedia.ImageORCarousel.length === 1 && (
-                      <Image
-                        src={activeData?.DesktopMedia.ImageORCarousel[0]?.url}
-                        alt={
-                          activeData?.DesktopMedia.ImageORCarousel[0]
-                            ?.alternativeText || ""
-                        }
-                        width={1905}
-                        height={1271}
-                        className={styles.mainMedia}
-                      />
-                    )}
-                </div>
-                <div className={styles.overlay}></div>
-                <div className={styles.content}>
-                  <div className={`${styles.child} ${styles.topLeft}`}>
-                    <ul className={styles.topNav}>
-                      <li>
-                        <Link href={"#"}>OVERVIEW</Link>
-                      </li>
-                      <li>
-                        <Link href={"#"}>MATERIAL APPROACH</Link>
-                      </li>
-                      <li>
-                        <Link href={"#"}>COLOURS AND TONES</Link>
-                      </li>
-                      <li>
-                        <Link href={"#"}>RELATED AESTHETICS </Link>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className={`${styles.child} ${styles.topRight}`}>
-                    <Paragraph>
-                      <BlocksRenderer content={activeData.Description || []} />
-                    </Paragraph>
-                  </div>
-                  <div className={`${styles.child} ${styles.bottomLeft}`}>
-                    <div className={`${styles.navItem}`}>
-                      {astheticsData.map((item) => {
-                        const isActive =
-                          pathname === `/aesthetics/${item.Slug}`;
-                        return (
-                          <Link
-                            href={item.Slug}
-                            className={isActive ? "activeLink" : "normalLink"}
-                          >
-                            <span className={styles.icon}>
-                              {isActive ? "(•)" : "( )"}
-                            </span>
-                            <span className={styles.label}>{item.Name}</span>
-                          </Link>
-                        );
-                      })}
-                    </div>
-                  </div>
-                  <div className={`${styles.child} ${styles.bottomRight}`}>
-                    <LinkWithArrow text={"SCROLL"} href={"#"} />
-                  </div>
-                  <div className={`${styles.child} ${styles.center}`}>
-                    <Heading level={1} className={styles.mainHeading}>
-                      {activeData.Name}
-                    </Heading>
-                  </div>
-                </div>
-              </section>
-              {activeData.Blocks.map((block, index) => (
-                <section
-                  key={index}
-                  className={`${styles.section} media-section`}
-                >
-                  <BlockRenderer block={block} />
-                </section>
-              ))}
-            </motion.div>
-          ) : (
-            <Loading />
-          )}
-        </motion.div>
+    <div className={styles.wrapper}>
+      <AnimatePresence>
+        {displayData && (
+          <motion.div
+            key="old-content"
+            initial={false}
+            animate={{ opacity: incomingData ? 1 : 1 }}
+            exit={{ opacity: 0 }}
+            style={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              top: 0,
+              left: 0,
+            }}
+          >
+            {renderContent(displayData)}
+          </motion.div>
+        )}
+
+        {incomingData && (
+          <motion.div
+            key="new-content"
+            initial={{ y: "100%" }}
+            animate={{ y: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.6, ease: [0.22, 1, 0.36, 1] }}
+            style={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              top: 0,
+              left: 0,
+            }}
+          >
+            {renderContent(incomingData)}
+          </motion.div>
+        )}
       </AnimatePresence>
     </div>
   );
