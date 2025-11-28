be300b2202754bd24c4936f25d461e36df462f6b
ramde solanki <ramdesolanki2511@gmail.com>
2025-11-28 18:34:26 +0530

    content page image slide navigation added

--- src/components/sections/ExpressiveMediaModule/index.js
+++ src/components/sections/ExpressiveMediaModule/index.js
@@ -1,18 +1,74 @@
+'use client';
+
 import MediaRenderer from "@/components/common/MediaRenderer";
 import TwoColumnLayout from "@/components/layouts/TwoColumnLayout";
 import Box from "@/components/ui/Box/Box";
 import Divider from "@/components/ui/Divider";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Pagination, Navigation } from 'swiper/modules';
+import Image from "next/image";
+import { useState } from "react";
+import styles from "./expressiveMedia.module.css";
 
 export default function ExpressiveMediaModule({ data }) {
   const { LeftSideMedia, RightSideMedia } = data;
+  const { EnableMuxVideo, MuxVideo, ImageORCarousel } = LeftSideMedia;
+  const [prevEl, setPrevEl] = useState(null);
+  const [nextEl, setNextEl] = useState(null);
+  const [paginationEl, setPaginationEl] = useState(null);
 
   const leftContent = (
     <>
-      <Box fullHeight direction="column" justify="space-between">
-        <div className="fullWidth">
-          <MediaRenderer media={LeftSideMedia} classes={'image'} />
+        <Box fullHeight direction="column" justify="space-between" mobileGap="90px" borderBottom mobileBorderBottom={false}>
+        <Swiper 
+              modules={[Pagination, Navigation]}
+              navigation={{
+                prevEl: prevEl,
+                nextEl: nextEl,
+              }}
+              pagination={{
+                el: paginationEl,
+                type: 'fraction',
+              }}
+              style={{ width: '100%', height: '100%' }}
+            >
+            {ImageORCarousel.map((img, index) => (
+              <SwiperSlide key={index}>
+                <Image
+                  src={img.url}
+                  alt={img?.alternativeText || ""}
+                  width={716}
+                  height={424}
+                  className="image"
+                />
+              </SwiperSlide>
+            ))}
+            </Swiper>
+      </Box>
+      <Box
+        fullHeight
+        direction="row"
+        justify="space-between"
+        align="flex-end"
+        padding="0"
+        equalChildren
+        showDivider={true}
+        hideMobileDivider={true}
+      >
+        <div className="p20">
+          <div className={styles.sliderNav}>
+            <button ref={(node) => setPrevEl(node)}>
+              Prev
+            </button>
+            <div ref={(node) => setPaginationEl(node)} className="text-center"></div>
+            <button ref={(node) => setNextEl(node)}>
+              Next
+            </button>
+          </div>
         </div>
-        <div> </div>
       </Box>
     </>
   );
