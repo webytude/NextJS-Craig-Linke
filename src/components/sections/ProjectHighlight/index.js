4c545c1e405a03e65c57c3af5d429fc515211779
ramde solanki <ramdesolanki2511@gmail.com>
2025-12-10 11:43:49 +0530

    project featured sticky added

--- src/components/sections/ProjectHighlight/index.js
+++ src/components/sections/ProjectHighlight/index.js
@@ -34,7 +34,7 @@ export default function ProjectHighlight({ data }) {
   const rightContent = (
     <>
       <div className="featuredProject p20">
-        <div className="flex justify-space-between pt20 pb20 no-padding-mobile">
+        <div className={`flex justify-space-between pt20 pb20 no-padding-mobile ${styles.stickyHeader}`}>
           <FadeUp><div className="text-light uppercase">{FeaturedProjects.Title}</div></FadeUp>
           {FeaturedProjects.Button && (
             <FadeUp>
