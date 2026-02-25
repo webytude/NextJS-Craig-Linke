516b98e081fb5ccd073cda8152a9ced490202e6e
Chandrika <chandrika.webytude@gmail.com>
2026-02-25 19:48:54 +0530

    add Google Tag Manager and Google Analytics scripts to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import { GLOBAL_QUERY } from "@/queries/queries";
 import LayoutClient from "./LayoutClient";
 import client from "@/lib/apolloClient";
 import SmoothScrolling from "@/components/common/SmoothScrolling";
+import Script from "next/script";
 
 
 export async function generateMetadata() {
@@ -14,6 +15,9 @@ export async function generateMetadata() {
   return {
     title: data.global.site_name,
     description: data.global.site_description,
+    verification: {
+      google: "e6bYik4ximfOekIUbF5_utfkZasYsBiW1HREVUGGDuc",
+    },
     icons: {
       icon: [{ url: data.global.site_favicon?.url || '/favicon.ico' }],
     },
@@ -30,7 +34,34 @@ export default async function RootLayout({ children }) {
 
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-RWESQMF571"
+          strategy="beforeInteractive"
+        />
+        <Script id="ga4-init" strategy="beforeInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-RWESQMF571');`}
+        </Script>
+        <Script id="gtm-init" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M7253PM8');`}
+        </Script>
+      </head>
       <body suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M7253PM8"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <SmoothScrolling />
         <LayoutClient globalData={data.global}>{children}</LayoutClient>
       </body>
