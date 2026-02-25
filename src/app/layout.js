6e3e57db8913ec8ea4d416b84f3329ab8a1c7921
ramde solanki <ramdesolanki2511@gmail.com>
2026-02-25 22:49:23 +0530

    gtm code added

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,7 +35,7 @@ export default async function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <Script
+        {/* <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-RWESQMF571"
           strategy="beforeInteractive"
         />
@@ -44,14 +44,23 @@ export default async function RootLayout({ children }) {
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-RWESQMF571');`}
-        </Script>
-        <Script id="gtm-init" strategy="beforeInteractive">
+        </Script> */}
+        {/* <Script id="gtm-init" strategy="beforeInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-M7253PM8');`}
-        </Script>
+        </Script> */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M7253PM8');`,
+          }}
+        />
       </head>
       <body suppressHydrationWarning>
         <noscript>
